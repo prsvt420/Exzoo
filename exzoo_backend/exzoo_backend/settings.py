4e64f6899db198fe23fe0380c43f596297e1861d
Denis Tokarev <wf.denis.tokarev@gmail.com>
2024-10-12 11:08:10 +0800

    Create Django project

---
+++ exzoo_backend/exzoo_backend/settings.py
@@ -0,0 +1,100 @@
+import os
+from pathlib import Path
+
+BASE_DIR: Path = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY: str = os.getenv('SECRET_KEY')
+
+DEBUG: bool = True
+
+ALLOWED_HOSTS: list[str] = [
+    '127.0.0.1',
+]
+
+CORS_ALLOWED_ORIGINS: list[str] = [
+    'http://localhost:3000',
+]
+
+INSTALLED_APPS: list[str] = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+]
+
+MIDDLEWARE: list[str] = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF: str = 'exzoo_backend.urls'
+
+TEMPLATES: list[dict] = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION: str = 'exzoo_backend.wsgi.application'
+
+DATABASES: dict = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_ADMIN'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS: list[dict] = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE: str = 'ru-RU'
+
+TIME_ZONE: str = 'Europe/Moscow'
+
+USE_I18N: bool = True
+
+USE_TZ: bool = True
+
+STATIC_URL: str = 'static/'
+
+MEDIA_URL: str = 'media/'
+MEDIA_ROOT: Path = BASE_DIR / 'media'
+
+DEFAULT_AUTO_FIELD: str = 'django.db.models.BigAutoField'
