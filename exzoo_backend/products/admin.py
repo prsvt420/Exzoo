5777b3c6374834ebf63c45e55c26ff19fcdc7ec1
Denis Tokarev <wf.denis.tokarev@gmail.com>
2024-10-13 10:06:45 +0800

    [products] Added model admin - ProductAdmin, CategoryAdmin, TagAdmin

--- exzoo_backend/products/admin.py
+++ exzoo_backend/products/admin.py
@@ -1,2 +1,38 @@
 from django.contrib import admin
 
+from products.models import Product, Category, Tag
+
+
+class BaseAdmin(admin.ModelAdmin):
+    fields: tuple = ('name', 'slug',)
+    list_display: tuple = ('name', 'slug')
+    search_fields: tuple = ('name',)
+    prepopulated_fields: dict = {'slug': ('name',)}
+    list_per_page: int = 25
+
+
+@admin.register(Product)
+class ProductAdmin(BaseAdmin):
+    fields: tuple = (
+        ('name', 'slug'),
+        ('description', 'country_of_origin'),
+        ('price', 'unit', 'discount_price',),
+        ('category', 'tags'),
+        'image',
+        'quantity',
+    )
+    list_display = ('name', 'price', 'discount_price', 'category', 'unit', 'quantity')
+    search_fields: tuple = ('name', 'description', 'country_of_origin')
+    list_filter: tuple = ('category', 'tags')
+    filter_horizontal: tuple = ('tags',)
+    list_editable: tuple = ('price', 'discount_price', 'unit', 'quantity')
+
+
+@admin.register(Category)
+class CategoryAdmin(BaseAdmin):
+    ...
+
+
+@admin.register(Tag)
+class TagAdmin(BaseAdmin):
+    ...
